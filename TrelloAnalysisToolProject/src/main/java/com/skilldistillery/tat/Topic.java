977d99ce980dab5287270dd70b994dd7fdfdc215
Emily Lau <emilylau@Brucebs-MacBook-Pro-2.local>
2019-10-04 17:10:48 -0600

    Fleshed out topic and and CsvFile entities, passed first CsvFileTest

--- TrelloAnalysisToolProject/src/main/java/com/skilldistillery/tat/Topic.java
+++ TrelloAnalysisToolProject/src/main/java/com/skilldistillery/tat/Topic.java
@@ -1,21 +1,60 @@
 package com.skilldistillery.tat;
 
+import java.util.List;
+
 public class Topic {
 
 	// F i e l d s
 	
-	private String instructor;
-	private String date; // This shouldn't be a String.
+	private List<String> instructors;
+	private String dateStr; // This shouldn't be a String.
 	private String topic;
 	
 	// C o n s t r u c t o r
-	
 	// ToDo: Write constructor
+	public Topic() {
+		super();
+	}
 	
+	
+
+	public Topic(List<String> instructors, String dateStr, String topic) {
+		super();
+		this.instructors = instructors;
+		this.dateStr = dateStr;
+		this.topic = topic;
+	}
+
+
+
 	// M e t h o d s
 	
 	// ToDo: Definitely add gets.
 	// ToDo: Maybe add sets (maybe not).
+
+	public List<String> getInstructors() {
+		return instructors;
+	}
+
+	public void setInstructors(List<String> instructors) {
+		this.instructors = instructors;
+	}
+
+	public String getDateStr() {
+		return dateStr;
+	}
+	public void setDateStr(String dateStr) {
+		this.dateStr = dateStr;
+	}
+	public String getTopic() {
+		return topic;
+	}
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+	
+	
+	
 	
 	
 }
--- TrelloAnalysisToolProject/src/main/java/com/skilldistillery/tat/TrelloCsvFile.java
+++ TrelloAnalysisToolProject/src/main/java/com/skilldistillery/tat/TrelloCsvFile.java
@@ -1,6 +1,10 @@
 package com.skilldistillery.tat;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TrelloCsvFile {
@@ -9,21 +13,66 @@ public class TrelloCsvFile {
 	
 	private String fileName;
 	
+	
 	// C o n s t r u c t o r s
 	
 	public TrelloCsvFile(String fileName) {
 		// ToDo: Write method - easy - 1 line?
+		this.setFileName(fileName);
 	}
 	
 	// M e t h o d s
 	
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
 	public List<Topic> getTopics() {
 		List<Topic> result = new ArrayList<>();
+		
+		BufferedReader buffIn = null;
+		try {
+		  buffIn = new BufferedReader(new FileReader("src/main/resources/" + fileName));
+		  String line;
+		  line = buffIn.readLine(); //to avoid the first row of the csv file - they're just labels
+		  while ((line = buffIn.readLine()) != null) {
+			  String[] currentLine =  line.split(",");
+			  System.out.println(Arrays.deepToString(currentLine));
+			  System.out.println("Current line length: " + currentLine.length);
+			  String[] topicInstructors = currentLine[3].split(",");
+			  List<String> topicInstructorsList = Arrays.asList(topicInstructors);
+			  String topicName = currentLine[1];
+			  String date = currentLine[14];
+			  
+			  Topic newTopic = new Topic(topicInstructorsList, date, topicName);
+			  result.add(newTopic);
+			  
+		  }
+		}
+		catch (IOException e) {
+		  System.err.println(e);
+		}
+		finally {
+		  if (buffIn != null) {
+		    try {
+		      buffIn.close();
+		    }
+		    catch (IOException e) {
+		      System.err.println(e);
+		    }
+		  }
+		}
 		
 		// ToDo: Read the file, fill in the List
 		//       This might use more methods in this class - private
 		//       methods.
-		
+		for (int i = 0; i < result.size(); i++) {
+			System.out.println(result.get(i));
+		}
 		return result;
 	}
 	
